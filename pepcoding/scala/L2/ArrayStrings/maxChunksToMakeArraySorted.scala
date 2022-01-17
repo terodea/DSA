cff20e522b55306c19e894ed1e1406b5f872e6fe
terodea <terodeakshay@gmail.com>
2022-01-17 23:56:45 +0530

    Scala code addition started

---
+++ pepcoding/scala/L2/ArrayStrings/maxChunksToMakeArraySorted.scala
@@ -0,0 +1,12 @@
+object maxChunksToMakeArraySorted{
+
+    def maxChunksToSorted(arr: Array[Int]): Int = {
+        val s = arr.scanLeft(0)((a, b) => a + (1<<b))
+        val p = (0 to arr.size-1).scanLeft(0)((a, b) => a + (1<<b))
+        (s zip p).map(a => a._1 == a._2).count(_ == true)-1
+    }
+    
+    def main(args: Array[String]): Unit{
+        var data = Array(4,3,2,1,0)
+    }
+}
